48058fc66062ade9627a642c8177488b7fe75549
Eugene <supernovaprotocol@gmail.com>
2023-01-25 15:58:40 +0700

    Inference script

---
+++ transcribe.py
@@ -0,0 +1,22 @@
+import argparse
+
+from PIL import Image
+from hwr_self_train.environment import TuningEnvironment
+from hwr_self_train.preprocessors import decode_output_batch
+from configuration import tokenizer
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Recognize text on a word image'
+    )
+    parser.add_argument('image_path', type=str, help='Path to the location of image containing word')
+    args = parser.parse_args()
+
+    env = TuningEnvironment()
+    recognizer = env.recognizer
+
+    image = Image.open(args.image_path)
+    label_distr = recognizer([image])
+    texts = decode_output_batch(label_distr, tokenizer)
+    print('Predicted text:', texts[0])
