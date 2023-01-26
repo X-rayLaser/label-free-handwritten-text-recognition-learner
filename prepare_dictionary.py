e6a5974e90c1a1837ae5ed6ee718f7cb0dd1ff6f
Eugene <supernovaprotocol@gmail.com>
2023-01-26 17:29:01 +0700

    Support building word frequencies from entire text corpora

--- prepare_dictionary.py
+++ prepare_dictionary.py
@@ -1,29 +1,53 @@
 import argparse
 import csv
+import os
 
+import nltk
 from nltk.probability import FreqDist
-from nltk.corpus import gutenberg
 
 
-def load_words(dict_file):
+def load_dict_file(dict_file):
+    """A generator yielding words from a dictionary text file"""
     with open(dict_file) as f:
-        return [word.strip() for word in f if word.strip()]
+        for word in f:
+            if word.strip():
+                yield word
 
 
-def filter_words(words, only_letters=True):
-    max_word_len = args.max_len
-    words = [w for w in words if len(w.strip()) <= max_word_len]
+def load_corpus(path):
+    with open(path) as f:
+        for line in f:
+            line = line.strip()
+            if line:
+                yield from nltk.word_tokenize(line)
 
-    if only_letters:
-        words = [w for w in words if w.isalpha()]
-    return words
+
+def load_corpora(corpora_dir):
+    """A generator yielding words contained in text files located within corpora_dir folder"""
+    for text_file in os.listdir(corpora_dir):
+        path = os.path.join(corpora_dir, text_file)
+        yield from load_corpus(path)
+
+
+def filter_words(words, allow_only_letters=True, max_word_len=14):
+    for w in words:
+        too_long = len(w.strip()) > max_word_len
+        has_non_letters = not w.isalpha()
+        if too_long or (allow_only_letters and has_non_letters):
+            continue
+        yield w
 
 
 def save_distr(words, destination):
     fdist = FreqDist()
 
-    for word in words:
+    for i, word in enumerate(words):
         fdist[word] += 1
+
+        if i % 100 == 0:
+            print(f'\rCounted words: {i + 1}', end='')
+
+    print('\nFrequency dictionary is complete')
 
     with open(destination, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',',
@@ -35,9 +59,15 @@ def save_distr(words, destination):
 
 
 def save_dict_file(words, destination):
-    unique = set(words)
+    vocab = set()
+    for i, w in enumerate(words):
+        vocab.add(w)
+        if i % 100 == 0:
+            print(f'\rBuilding vocab. Words processed: {i + 1}', end='')
+
+    print('\nVocab is built')
     with open(destination, 'w') as f:
-        file_content = '\n'.join(unique)
+        file_content = '\n'.join(vocab)
         f.write(file_content)
 
 
@@ -51,8 +81,9 @@ if __name__ == '__main__':
     parser.add_argument('--dict-file', type=str, default='',
                         help='Location of a dictionary file (required if --gutenberg is omitted)')
 
-    parser.add_argument('--gutenberg', default=False, action='store_true',
-                        help='When set, will use NLTK corpus (gutenberg) to extract words.')
+    parser.add_argument('--corpora-dir', type=str, default='',
+                        help='Generate words from a folder of text files')
+
     parser.add_argument('--max-len', type=int, default=14,
                         help='Exclude words from the output file longer than max-len characters')
     parser.add_argument('--only-letters', default=False, action='store_true',
@@ -62,9 +93,9 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    all_words = gutenberg.words() if args.gutenberg else load_words(args.dict_file)
+    words_gen = load_corpora(args.corpora_dir) if args.corpora_dir else load_dict_file(args.dict_file)
 
-    filtered_words = filter_words(all_words, args.only_letters)
+    words_gen = filter_words(words_gen, args.only_letters, max_word_len=args.max_len)
 
     save_func = save_distr if args.with_freq else save_dict_file
-    save_func(filtered_words, args.output_file)
+    save_func(words_gen, args.output_file)
