5a81a7cbe016fba57cc4e783bac5c434ca0c05d1
Eugene <supernovaprotocol@gmail.com>
2023-01-27 18:51:23 +0700

    Logging config

--- hwr_self_train/__init__.py
+++ hwr_self_train/__init__.py
@@ -1,2 +1,17 @@
 import logging.config
-logging.config.fileConfig('logging.conf')
+
+logger = logging.getLogger(__name__)
+
+logger.setLevel(logging.DEBUG)
+
+handler = logging.FileHandler('hwr_self_training.log')
+handler.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+handler.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(handler)
