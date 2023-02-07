10d543ffc2f167cf94e3c34d0af05794073f1486
Eugene <supernovaprotocol@gmail.com>
2023-02-07 22:14:59 +0700

    Fix evaluate script

--- evaluate.py
+++ evaluate.py
@@ -1,10 +1,21 @@
+import argparse
 from hwr_self_train.environment import Environment
 from hwr_self_train.evaluation import evaluate
 from hwr_self_train.formatters import Formatter
+from hwr_self_train.checkpoints import SessionDirectoryLayout
 
 
 if __name__ == '__main__':
-    env = Environment()
+    parser = argparse.ArgumentParser(
+        description='Evaluate a model'
+    )
+
+    parser.add_argument('session_dir', type=str,
+                        help='Path to the location of session directory')
+    args = parser.parse_args()
+    config = SessionDirectoryLayout(args.session_dir).load_config()
+    env = Environment(config)
+
     metrics = {}
     for task in env.eval_tasks:
         metrics.update(evaluate(task))
